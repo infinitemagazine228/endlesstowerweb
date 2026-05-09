a2b828ab67c131f1af97778e3bd573bf60c68973
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-09 23:09:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-// server.js
 const express = require('express');
-const http = require('http');
 const path = require('path');
+const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// phục vụ frontend từ thư mục public
+// phục vụ file index.html trong thư mục public
 app.use(express.static(path.join(__dirname, 'public')));
 
 let players = {};
@@ -51,3 +50,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
